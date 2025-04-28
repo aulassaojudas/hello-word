ada015f2cf9dba525383e486a77912fb2bd44feb
Carmino <prof.carmino@gmail.com>
2025-04-28 07:26:30 -0300

    Estrutura do projeto css, html, pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Header } from './components/Header/Header';
+import { Nav } from './components/Nav/Nav';
+import { Aside } from './components/Aside/Aside';
+import { HelloWorld } from './components/pages/HelloWorldProps';
+import { Footer } from './components/Footer/Footer';
+
+import { Sobre } from './components/pages/Sobre';
+import { Login } from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="grid-container">
+        <Header />
+        <Nav />
+        <Aside />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<HelloWorld nome="Carmino" />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
